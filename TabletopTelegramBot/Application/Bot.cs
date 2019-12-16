066cf2d241a1a6131919ea8d50e589dbe593b012
Anton Borodin <boroda132@hotmail.com>
2019-12-16 13:59:03 +0300

    init

--- TabletopTelegramBot/Application/Bot.cs
+++ TabletopTelegramBot/Application/Bot.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using TabletopTelegramBot.Models;
 using TabletopTelegramBot.Settings;
